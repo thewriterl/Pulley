97205f3f98c505da1b0a4668ba982022de53b983
Luiz Fernando França <luiz.darthvader@gmail.com>
2018-11-27 19:47:07 -0200

    Initial Commit

---
+++ NewPulley/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewPulley
+//
+//  Created by Luiz Fernando França on 27/11/18.
+//  Copyright © 2018 Luiz Fernando França. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NewPulleyTests/NewPulleyTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewPulleyTests.swift
+//  NewPulleyTests
+//
+//  Created by Luiz Fernando França on 27/11/18.
+//  Copyright © 2018 Luiz Fernando França. All rights reserved.
+//
+
+import XCTest
+@testable import NewPulley
+
+class NewPulleyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
